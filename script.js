8a521909f4e01886ccd7be093dfdee24dab0f0dc
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-02-09 19:14:39 +0500

    Finished only css remaining

--- script.js
+++ script.js
@@ -7,18 +7,52 @@ var milliseconds = 0;
 
 var hoursDisp = document.getElementById("hoursDisp");
 var minsDisp = document.getElementById("minsDisp");
+var secondsDisp = document.getElementById('secondsDisp');
+var millisecondsDisp =document.getElementById('millisecondsDisp');
 
-
+// console.log(hoursDisp);
 
 function renderTimer(){
     hoursDisp.innerHTML = hours;
     console.log(hoursDisp)
     console.log(hoursDisp.innerHTML)
+    minsDisp.innerHTML = mins;
+    secondsDisp.innerHTML=seconds;
+    millisecondsDisp.innerHTML = milliseconds;
 }
+// renderTimer()
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        milliseconds++;
+        if(milliseconds==10){
+            milliseconds = 0;
+            seconds++;
+            if(seconds==60){
+                seconds=0;
+                mins++;
+                if(mins==60){
+                    mins=0;
+                    hours++;
+                }
+            }
+    
+        }
+        renderTimer();
+    },100);
 
-renderTimer()
-
-
+}
+function stopTimer(){
+    clearInterval(interval);
+}
+function resetTimer(){
+    stopTimer();
+    hours=0;
+    mins=0;
+    seconds=0;
+    milliseconds=0;
+    renderTimer();
+}
 
 
 
